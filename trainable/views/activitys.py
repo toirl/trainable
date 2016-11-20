83abe693c9492b0f13ab09e9570ad436378a3a84
Torsten Irländer <torsten.irlaender@googlemail.com>
2016-11-20 12:12:26 +0100

    Update activity when opening it.

--- trainable/views/activitys.py
+++ trainable/views/activitys.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from pyramid.view import view_config
 from ringo.lib.helpers import get_action_routename
-from ringo.views.base import update
+from ringo.views.base import update, read
 from ringo.views.request import get_item_from_request
 from ringo.views.response import JSONResponse
 
@@ -18,6 +18,31 @@ def syncwithstrava(request):
     msg = _('The activity was successfully synchronized with Strava')
     request.session.flash(msg, "success")
     return update(request)
+
+
+@view_config(route_name=get_action_routename(Activity, "update"), renderer="/default/update.mako")
+def _update(request):
+    """If activity is not completely synced. Than sync it when opening"""
+    item = get_item_from_request(request)
+    if not item.has_streams:
+        request.item = sync_activity(request)
+        _ = request.translate
+        msg = _('The activity was successfully synchronized with Strava')
+        request.session.flash(msg, "success")
+    return update(request)
+
+
+@view_config(route_name=get_action_routename(Activity, "read"), renderer="/default/read.mako")
+def _read(request):
+    """If activity is not completely synced. Than sync it when opening"""
+    item = get_item_from_request(request)
+    if not item.has_streams:
+        request.item = sync_activity(request)
+        _ = request.translate
+        msg = _('The activity was successfully synchronized with Strava')
+        request.session.flash(msg, "success")
+    return read(request)
+
 
 def a2gj(item):
     x2coord = {}
