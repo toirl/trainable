eb7a3bc498e31967d2cd048035e920b68098bd23
Torsten Irländer <torsten.irlaender@googlemail.com>
2016-12-01 21:58:08 +0100

    Added basic fields for the trainingplan

--- trainable/model/trainingplan.py
+++ trainable/model/trainingplan.py
@@ -2,10 +2,17 @@ import sqlalchemy as sa
 from ringo.model import Base
 from ringo.model.base import BaseItem
 from ringo.model.mixins import (
-Owned
+    Owned
 )
+
 
 class Trainingplan(BaseItem, Owned, Base):
     __tablename__ = 'trainingplans'
     _modul_id = 1001
     id = sa.Column(sa.Integer, primary_key=True)
+
+    name = sa.Column('name', sa.String, nullable=False, default='')
+    highlight_1_date = sa.Column('highlight_1_date', sa.Date)
+    highlight_1_title = sa.Column('highlight_1_title', sa.String, nullable=False, default='')
+    highlight_1_desc = sa.Column('highlight_1_desc', sa.String, nullable=False, default='')
+    weekly_hours = sa.Column('weekly_hours', sa.Integer, nullable=False)
