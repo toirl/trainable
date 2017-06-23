866bff1e74c6c2504290ba70e1e6f509515861c3
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-06-23 14:34:31 +0200

    Added requirements.

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 import os
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements(os.path.join(here, 'requirements.txt'), session=False)
-
-# reqs is a list of requirement
-# e.g. ['ringo', 'ringo_printtemplates']
-requires = [str(ir.req) for ir in install_reqs]
 
 setup(name='trainable',
       version='0.1',
@@ -31,7 +24,11 @@ setup(name='trainable',
       include_package_data=True,
       zip_safe=False,
       test_suite='trainable',
-      install_requires=requires,
+      install_requires=[
+          "ringo",
+          "requests",
+          "stravalib"
+      ],
       entry_points="""\
       [paste.app_factory]
       main = trainable:main
